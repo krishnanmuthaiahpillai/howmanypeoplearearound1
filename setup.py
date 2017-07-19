538b2feaed0323ddc970c814bf312b72ba20d6b7
krishnan <krishnapillai87@gmail.com>
2017-07-19 11:42:18 +0530

    First commit

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='howmanypeoplearearound1',
+    packages=['howmanypeoplearearound1'],
+    version='0.0.1',
+    description='A tshark wrapper to count the number of cellphones in the vicinity',
+    author='krishnan',
+    url='https://github.com/krishnanmuthaiahpillai/howmanypeoplearearound1',
+    author_email='krishnanmuthaiahpillai@gmail.com',
+    download_url='https://github.com/schollz/howmanypeoplearearound/archive/v0.3.5.tar.gz',
+    keywords=['tshark', 'wifi', 'location'],
+    classifiers=[],
+    install_requires=[
+        "click",
+        "netifaces",
+        "pick",
+    ],
+    setup_requires=[],
+    tests_require=[],
+    entry_points={'console_scripts': [
+        'howmanypeoplearearound1 = howmanypeoplearearound1.__main__:main',
+    ], },
+)
